a478f961736f3f38aad059d4c404bc74dcfb7caf
robot10 <--->
2024-08-09 15:04:29 +0200

    Added some weapons

--- init.lua
+++ init.lua
@@ -47,3 +47,47 @@ minetest.register_tool("strange_weapons:mud_ball", {
 	groups = {sword = 1}
 })
 
+minetest.register_tool("strange_weapons:crucifix", {
+    description = "Crucifix",
+    inventory_image = "crucifix.png",
+	tool_capabilities = {
+		full_punch_interval = 0.6,
+		max_drop_level=1,
+		groupcaps={
+					snappy={times={[3]=0.5}, uses=10, maxlevel=3},
+		},
+		damage_groups = {fleshy=40},
+	},
+	sound = {breaks = "default_tool_breaks"},
+	groups = {sword = 1}
+})
+
+minetest.register_tool("strange_weapons:the_leaf", {
+    description = "The Leaf",
+    inventory_image = "the_leaf.png",
+	tool_capabilities = {
+		full_punch_interval = 0.6,
+		max_drop_level=1,
+		groupcaps={
+					snappy={times={[3]=3}, uses=50, maxlevel=0},
+		},
+		damage_groups = {fleshy=2},
+	},
+	sound = {breaks = "default_dig_snappy"},
+	groups = {sword = 1}
+})
+
+minetest.register_tool("strange_weapons:cracked_wood", {
+    description = "Cracked Wood",
+    inventory_image = "cracked_wood.png",
+	tool_capabilities = {
+		full_punch_interval = 0.6,
+		max_drop_level=1,
+		groupcaps={
+					snappy={times={[3]=3}, uses=10, maxlevel=0},
+		},
+		damage_groups = {fleshy=2},
+	},
+	sound = {breaks = "default_tool_breaks"},
+	groups = {sword = 1}
+})
